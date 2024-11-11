4f13f31c8e6aa1dbb6a540375c45c09a1e57cc94
Asifullah12592 <Asifullah12592@gmail.com>
2024-11-11 07:42:33 +0500

    My Portfolio

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,18 +4,20 @@ import Navbar from "@/components/Navbar";
 const Main = () => {
   return (
     <div id='hero' className='min-h-screen bg-no-repeat bg-[url(/Asif.png)] bg-cover'
-    style={{backgroundSize:"35%", backgroundPosition: "left 100px top 100px"}} 
+    style={{backgroundSize:"35%", backgroundPosition: "right 100px top 100px"}} 
     >
       <Navbar />
       <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
-        <div className='hidden lg:block'></div>
+        
         <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center'>
           <div>
-            <p  data-aos="zoom-in">I'm</p>
-            <p  data-aos="zoom-in">Hafiz</p>
-            <p  data-aos="zoom-in">Asifullah</p>
+            <p className=""data-aos="zoom-in">I am</p>
+            <p  data-aos="fade-right"data-aos-offset="300"data-aos-easing="ease-in-sine" style={{color:'#f80606'}}>Hafiz</p>
+          
+            <p data-aos="zoom-in">Asifullah</p>
           </div>
         </div>
+        <div className='hidden lg:block'></div>
       </div>
     </div>
   )
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,7 +6,7 @@ const Skills = () => {
             <div className='grid md:grid-cols-2 gap-20 items-center'>
                 <div  data-aos="zoom-in">
                     <h2 className='text-4xl md:text-5xl'>My Skills</h2>
-                    <p className='text-gray-500 pt-2'>I am currently seeking an internship or a junior web developer position where I can leverage my skills to help achieve the company's digital goals. My goal is to become a professional web developer and work in a company that supports my growth and learning.</p>
+                    <p className='text-gray-500 pt-2'>I am currently seeking an internship or a junior web developer position where I can leverage my skills to help achieve the company digital goals. My goal is to become a professional web developer and work in a company that supports my growth and learning.</p>
                 </div>
                 <div >
                     <div className='grid grid-cols-2 text-accent text-3xl sm:text-4xl'>
